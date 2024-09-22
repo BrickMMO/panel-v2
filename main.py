14593a58ef8fa64352065ba74a59d0acf3380efb
Pavan Mistry <pavan32100@gmail.com>
2024-09-21 23:30:51 -0400

    Added ON OFF sounds

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 ev3 = EV3Brick()
+ev3.speaker.set_speech_options(
+    language="en-us", voice="f2", speed=30, pitch=50)
+ev3.speaker.set_volume(volume=5, which='_all_')
 
 # Initialize motors and sensor
 mainMotorSwitch = Motor(Port.A, Direction.CLOCKWISE)
@@ -19,9 +22,7 @@ switch2 = TouchSensor(Port.S2)
 switch3 = TouchSensor(Port.S3)
 switch4 = TouchSensor(Port.S4)
 
-# Move the motor to a known position (such as fully counterclockwise)
-# You can adjust the `target_angle=-180` depending on how far back you want the motor to go.
-# Move backward until it stalls
+# Move motor until stalled to make a default position
 mainMotorSwitch.run_until_stalled(speed=-100, then=Stop.HOLD, duty_limit=20)
 initialMainMotorAngle = mainMotorSwitch.angle()
 
@@ -29,7 +30,7 @@ initialMainMotorAngle = mainMotorSwitch.angle()
 print("Initial Angle:", initialMainMotorAngle)
 
 mainMotorSwitch.run_target(
-    speed=500, target_angle=initialMainMotorAngle + 64, then=Stop.COAST, wait=True)
+    speed=500, target_angle=initialMainMotorAngle + 64, then=Stop.HOLD, wait=True)
 # Now, reset the motor's angle to zero after it has reached the physical limit
 mainMotorSwitch.reset_angle(0)
 
@@ -41,17 +42,20 @@ def toggle_motor_position():
     print("Current motor ANGLE: ", angle)
 
     # If the motor is moved away from 0, move it to 60 degrees (ON)
-    if (angle >= -8 and angle <= -3) or (angle >= 3 and angle <= 8):
+    if (angle >= -8 and angle <= -3) or (angle >= 3 and angle <= 15):
         # if angle > 2 and angle <= 89:
         mainMotorSwitch.run_target(
-            speed=500, target_angle=60, then=Stop.COAST, wait=True)
+            speed=500, target_angle=60, then=Stop.BRAKE, wait=True)
+        ev3.speaker.play_notes(['C4/8', 'E4/8', 'G4/4'], 300)
         return True
 
     # If the motor is moved away from 60, move it back to 0 degrees (OFF)
-    elif angle <= 55 or angle >= 65:
+    elif (angle >= 50 and angle <= 57) or (angle >= 63 and angle <= 68):
         mainMotorSwitch.run_target(
-            speed=-500, target_angle=0, then=Stop.COAST, wait=True)
+            speed=-500, target_angle=0, then=Stop.BRAKE, wait=True)
+        ev3.speaker.play_notes(['G4/8', 'E4/8', 'C4/4'], 120)
         return False
+
     # Motor is either at 0 or 60 degrees
     return angle >= 59 or angle <= 62
 
@@ -69,7 +73,7 @@ while True:
         if switch2.pressed():
             # Print message and play a beep when pressed
             print("Switch is pressed!")
-            ev3.speaker.beep(frequency=1000, duration=500)
+            ev3.speaker.beep()
     else:
         # Motor is OFF, no actions taken
         pass
